c412c0d5ec2542c7eb8193b13287750dd651f93d
parseley <gruns0989@naver.com>
2024-10-09 19:24:02 +0900

    [feat] Add scoring system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public TileBoard board;
     public CanvasGroup gameOver;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI hiscoreText;
+    private int score;
 
     private void Start()
     {
@@ -14,6 +18,9 @@ public class GameManager : MonoBehaviour
 
     public void NewGame()
     {
+
+        SetScore(0);
+        hiscoreText.text = LoadHiscore().ToString();
 
         gameOver.alpha = 0F;
         gameOver.interactable = false;
@@ -28,23 +35,49 @@ public class GameManager : MonoBehaviour
     {
         board.enabled = false;
         gameOver.interactable = true;
-        
+
         StartCoroutine(Fade(gameOver, 1F, 1F));
     }
 
-    private IEnumerator Fade(CanvasGroup canvasGroup, float to, float delay) {
+    private IEnumerator Fade(CanvasGroup canvasGroup, float to, float delay)
+    {
         yield return new WaitForSeconds(delay);
 
         float elapsed = 0F;
         float duration = 0.5F;
         float from = canvasGroup.alpha;
 
-        while (elapsed < duration) {
+        while (elapsed < duration)
+        {
             canvasGroup.alpha = Mathf.Lerp(from, to, elapsed / duration);
             elapsed += Time.deltaTime;
             yield return null;
         }
 
         canvasGroup.alpha = to;
+    }
+
+    public void IncreaseScore(int points)
+    {
+        SetScore(score + points);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        scoreText.text = score.ToString();
+        SaveHiscore();
+    }
+
+    private void SaveHiscore()
+    {
+        int hiscore = LoadHiscore();
+
+        if (score > hiscore) PlayerPrefs.SetInt("hiscore", score);
+    }
+
+    private int LoadHiscore()
+    {
+        return PlayerPrefs.GetInt("hiscore", 0);
     }
 }
